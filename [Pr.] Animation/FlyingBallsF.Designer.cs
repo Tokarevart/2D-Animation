700758808d991df501a6dffa300ce703c92e7dff
Tokarev Artem Alekseevich <tokarev_28@mail.ru>
2017-07-05 11:51:42 +0800

    Initial commit

---
+++ [Pr.] Animation/FlyingBallsF.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace _Pr.__Animation
+{
+    partial class FlyingBallsF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.сменитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UpdFreq = new System.Windows.Forms.NumericUpDown();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ElastCoef = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UpdFreq)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ElastCoef)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сменитьToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1199, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // сменитьToolStripMenuItem
+            // 
+            this.сменитьToolStripMenuItem.Name = "сменитьToolStripMenuItem";
+            this.сменитьToolStripMenuItem.Size = new System.Drawing.Size(135, 20);
+            this.сменитьToolStripMenuItem.Text = "Сменить вид анимации";
+            this.сменитьToolStripMenuItem.Visible = false;
+            this.сменитьToolStripMenuItem.Click += new System.EventHandler(this.сменитьToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.ElastCoef);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.StopBtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.UpdFreq);
+            this.panel1.Controls.Add(this.StartBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1199, 45);
+            this.panel1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(802, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(388, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Нажмите левую кнопку мыши для изменения направления движения шара";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(802, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(392, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Нажмите правую кнопку мыши для изменения начального положения шара";
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(84, 10);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 4;
+            this.StopBtn.Text = "Стоп";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Интервал обновления";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "мс";
+            // 
+            // UpdFreq
+            // 
+            this.UpdFreq.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.UpdFreq.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UpdFreq.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UpdFreq.Location = new System.Drawing.Point(309, 10);
+            this.UpdFreq.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.UpdFreq.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.UpdFreq.Name = "UpdFreq";
+            this.UpdFreq.Size = new System.Drawing.Size(61, 23);
+            this.UpdFreq.TabIndex = 1;
+            this.UpdFreq.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(3, 10);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Запуск";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1199, 596);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // ElastCoef
+            // 
+            this.ElastCoef.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ElastCoef.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ElastCoef.DecimalPlaces = 2;
+            this.ElastCoef.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ElastCoef.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.ElastCoef.Location = new System.Drawing.Point(528, 11);
+            this.ElastCoef.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.ElastCoef.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.ElastCoef.Name = "ElastCoef";
+            this.ElastCoef.Size = new System.Drawing.Size(61, 23);
+            this.ElastCoef.TabIndex = 7;
+            this.ElastCoef.ThousandsSeparator = true;
+            this.ElastCoef.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(398, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Коэфициент упругости";
+            // 
+            // FlyingBallsF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 665);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1215, 703);
+            this.MinimumSize = new System.Drawing.Size(1215, 703);
+            this.Name = "FlyingBallsF";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Симуляция полёта шара";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UpdFreq)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ElastCoef)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сменитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown UpdFreq;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown ElastCoef;
+    }
+}
---
+++ [Pr.] Animation/FlyingBallsF.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace _Pr.__Animation
+{
+    public partial class FlyingBallsF : Form
+    {
+        private bool TimerStarted = false, flag;
+        private double nowYd = 0, nowXd = 0, prevYd;
+        private double velX, velY = 0, dY, dX, Y0, X0;
+        private int x0 = 50, y0 = 300, ex = 50, ey = 300;
+        private Bitmap bmp;
+        private Graphics g;
+        private SolidBrush br = new SolidBrush(Color.Red);
+        private Rectangle r;
+        private Timer tmr = new Timer();
+        public FlyingBallsF()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            r = new Rectangle(x0 - 13, y0 - 13, 26, 26);
+            g.FillEllipse(br, r);
+            g.DrawEllipse(Pens.Black, r);
+        }
+
+        private void сменитьToolStripMenuItem_Click(object sender, EventArgs e){}
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработчик: Токарев А.А.");
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (TimerStarted == false && e.Button == MouseButtons.Left)
+            {
+                g.Clear(Color.White);
+                g.FillEllipse(br, r);
+                g.DrawEllipse(Pens.Black, r);
+                ex = e.X;
+                ey = e.Y;
+                Pen mypen = new Pen(Color.Black, 5);
+                mypen.StartCap = LineCap.RoundAnchor;
+                mypen.EndCap = LineCap.ArrowAnchor;
+                g.DrawLine(mypen, x0, y0, ex, ey);
+                pictureBox1.Refresh();
+            }
+            if (TimerStarted == false && e.Button == MouseButtons.Right)
+            {
+                x0 = e.X;
+                y0 = e.Y;
+                g.Clear(Color.White);
+                r = new Rectangle(x0 - 13, y0 - 13, 26, 26);
+                g.FillEllipse(br, r);
+                g.DrawEllipse(Pens.Black, r);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            TimerStarted = false;
+            tmr.Stop();
+            ex = x0;
+            ey = y0;
+            g.Clear(Color.White);
+            r = new Rectangle(x0 - 13, y0 - 13, 26, 26);
+            g.FillEllipse(br, r);
+            g.DrawEllipse(Pens.Black, r);
+            pictureBox1.Refresh();
+        }
+        
+
+        private int NowX ()
+        {
+            nowXd = X0 + 1.7 * dX / 200.0;
+            return (int)Math.Truncate(nowXd);
+        }
+        
+        private int NowY ()
+        {
+            prevYd = nowYd;
+            nowYd = Y0 + 1.7 * dY / 8000;
+            return (int)Math.Truncate(nowYd);
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (TimerStarted == true)
+            {
+                TimerStarted = false;
+                tmr.Stop();
+            }
+            flag = false;
+            tmr = new Timer();
+            g.Clear(Color.White);
+            tmr.Interval = (int)UpdFreq.Value;
+            int x = x0;
+            int y = y0;
+            int tx = 0, ty = 0;
+            Y0 = y0;
+            X0 = x0;
+            velX = ex - x0;
+            velY = (ey - y0) * 40;
+            r = new Rectangle(x0 - 13, y0 - 13, 26, 26);
+            g.FillEllipse(br, r);
+            g.DrawEllipse(Pens.Black, r);
+            tmr.Tick += new EventHandler((o, ev) =>
+            {
+                if (y >= pictureBox1.Height - 13 || y <= 0)
+                {
+                    velY = (-1 * velY - 9.81 * 10 * (ty - tmr.Interval)) * (double)ElastCoef.Value;
+                    ty = 0;
+                    if (y >= pictureBox1.Height - 13)
+                    {
+                        Y0 = prevYd;
+                        if (prevYd >= pictureBox1.Height - 13) flag = true;
+                    }
+                    else Y0 = prevYd;
+                }
+                if (x >= pictureBox1.Width || x <= 13)
+                {
+                    velX = -velX * (double)ElastCoef.Value;
+                    tx = 0;
+                    if (x >= pictureBox1.Width) X0 = pictureBox1.Width - 13;
+                    else X0 = 13;
+                }
+                ty += tmr.Interval;
+                tx += tmr.Interval;
+                dX = velX * tx;
+                dY = 10 * (9.81 / 2) * ty * ty + velY * ty;
+                x = NowX();
+                y = NowY();
+                g.Clear(Color.White);
+                if (flag == false) r = new Rectangle(x - 13, y - 13, 26, 26);
+                else r = new Rectangle(x - 13, pictureBox1.Height - 26, 26, 26);
+                g.FillEllipse(br, r);
+                g.DrawEllipse(Pens.Black, r);
+                pictureBox1.Refresh();
+            });
+            TimerStarted = true;
+            tmr.Start();
+        }
+    }
+}
